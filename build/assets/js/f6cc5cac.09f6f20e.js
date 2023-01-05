5f479d8ebf05bc782fb39911ea87c2059442f1f4
jevilstail <100316907+commitblue@users.noreply.github.com>
2023-01-05 20:02:45 +0200

    blue docs????

---
+++ build/assets/js/f6cc5cac.09f6f20e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[250],{3891:e=>{e.exports=JSON.parse('{"sourceUrl":"https://github.com/commitblue/customclickdetector/blob/master","baseUrl":"/customclickdetector/","classOrder":[],"apiCategories":[]}')}}]);
