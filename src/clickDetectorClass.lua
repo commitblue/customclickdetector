20137ad460c49b4479a71b1edac06d41c1e85bee
jevilstail <100316907+commitblue@users.noreply.github.com>
2023-01-05 19:57:32 +0200

    s

--- src/clickDetectorClass.lua
+++ src/clickDetectorClass.lua
@@ -1,11 +1,5 @@
 --!strict
 local fastSignal = require(script.Parent.Packages.fastSignal)
---[=[
-    @class clickDetectorClass
-
-    This class is created by module.new.
-    Class that contains hovericon, events.
-]=]
 local clickDetectorClass = {}
 clickDetectorClass.__index = clickDetectorClass
 function clickDetectorClass:_returnSelf()
@@ -27,12 +21,6 @@ function clickDetectorClass:_cleanup()
         clickDetectorClass.mouseLeave
     )
 end
---[=[
-    @within clickDetectorClass
-    Whenever you parent an object to another one, this function is handy.
-    
-    @param instance Instance
-]=]
 function clickDetectorClass:changeInstance(instance)
     self.instance = instance
 end
